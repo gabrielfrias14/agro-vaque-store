e5024112fbde360990a378f5830a3bbbc7d63117
Guilhermelow <melo66659@gmail.com>
2025-11-17 20:21:47 -0300

    configura shadcnui

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+
+import { Button } from "@/components/ui/button"
+import "../globals.css"
+import { Input } from "@/components/ui/input";
+export default function Dashboard() {
+  return (
+    <div >
+      <h1>pagina de Dashboard</h1>
+      <Button>dsad</Button>
+      <Input></Input>
+    </div>
+  );
+}
